5ae6446c87848971ecffdcbd06f763471442a277
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-22 16:58:09 +0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,95 @@
-import type { NextPage } from 'next'
+import { Tab } from '@headlessui/react'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
+import Landing from '../components/Landing'
+import { fetchCategories } from '../utils/fetchCategories'
 
-const Home: NextPage = () => {
+  interface Props {
+    categories: Category []
+  }
+
+const Home = ({ categories}: Props) => {
+  console.log(categories);
+  
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Apple Redesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header />
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className='relative h-[200vh] bg-[#e7ecee]'> 
+        <Landing />
       </main>
+      
+      <section className='relative z-40 -mt-[100vh] min-h-screen bg-[#181818]'>
+        <div className='py-16 space-y-10'>
+          <h1 className='text-4xl font-medium tracking-wide text-center text-white md:text-5xl'>
+          New Promos
+          </h1>   
+          
+          <Tab.Group>
+            {/* <Tab.List className="flex justify-center">
+              {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  { category.title}
+                </Tab>
+              ))}
+            </Tab.List> */}
+            <Tab.Panels className="pt-10 pb-24 mx-auto max-w-fit sm:px-4">
+              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+
+//Backend Code
+
+// export const getServerSideProps: GetServerSideProps<Props> = async () => {
+//     const categories = await fetchCategories()
+
+//   return {
+//     props: {
+//       categories,
+//     },
+//   };
+// }
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const categories = await fetchCategories();
+  // const products = await fetchProducts();
+  // const session = await getSession(context);
+
+  return {
+    props: {
+      categories,
+      // products,
+      // session,
+    },
+  };
+};
